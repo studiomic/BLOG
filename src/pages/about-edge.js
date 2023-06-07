2fa0e155e34aac7a77232942aba94b22bb7dc300
StudioMic <jnl@studiomic.net>
2023-06-07 12:54:39 +0900

    0607

--- src/pages/about-edge.js
+++ src/pages/about-edge.js
@@ -1,30 +1,36 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import * as styles from '../styles/note.module.scss'
 
 const AboutPage = ({ data }) => {
 console.log(data);
 return (
 <Layout>
-	<h1>edge.node.</h1>
-	<table>
-	<thead>
-		<tr>
-		<th>パス</th>
-		<th>サイズ</th>
-		<th>作成時間</th>
-		</tr>
-	</thead>
-	<tbody>
-		{data.allFile.edges.map((edge) => (
-		<tr kye={edge.node.ctime}>
-			<td>{edge.node.relativePath}</td>
-			<td>{edge.node.size}</td>
-			<td>{edge.node.ctime}</td>
-		</tr>
-		))}
+	<div className={styles.container}>
+        <div className={styles.article}>
+			<h1>edge.node.</h1>
+			<table>
+			<thead>
+				<tr>
+				<th>パス</th>
+				<th>サイズ</th>
+				<th>作成時間</th>
+				</tr>
+			</thead>
+			<tbody>
+			{data.allFile.edges.map((edge) => (
+			<tr kye={edge.node.ctime}>
+				<td>{edge.node.relativePath}</td>
+				<td>{edge.node.size}</td>
+				<td>{edge.node.ctime}</td>
+			</tr>
+			))}
 	</tbody>
 	</table>
+	</div>
+	</div>
+
 </Layout>
 );
 };
