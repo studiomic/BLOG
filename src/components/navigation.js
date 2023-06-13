99e77fb0ffb36c37ec0917ec6bf5a1bd8e20152f
StudioMic <jnl@studiomic.net>
2023-06-13 19:37:51 +0900

    0613

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // import DarkModeToggle from './DarkModeToggle';
 import * as styles from '../styles/navigation.module.scss'
-import IMG from "../asset/img/logo-wt.png"
+// import IMG from "../asset/img/logo-wt.png"
 // import IMG2 from "../asset/svg/iconmonstr-weather-2.svg"
 // import IMG3 from "../asset/svg/moon-solid.svg"
 // import IMG4 from "../asset/svg/iconmonstr-star-filled.svg"
@@ -14,7 +16,15 @@ import IMG from "../asset/img/logo-wt.png"
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
-      <img src={IMG} alt="Style de Cerise" />
+    <StaticImage
+      src="../asset/img/Logo640.webp"
+      alt="LOGO"
+      placeholder="blurred"
+      layout="fixed"
+      width={200}
+      // height={373}
+    />
+      {/* <img src={IMG} alt="Style de Cerise" /> */}
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
