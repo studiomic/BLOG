b177470ef63679d96dd3b27328b0540687f7b2d8
StudioMic <jnl@studiomic.net>
2023-06-21 20:01:51 +0900

    0621

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -4,6 +4,7 @@ import useDarkMode from 'use-dark-mode';
 
 const DarkModeToggle = () => {
 	const darkMode = useDarkMode(false);
+	// const darkMode = useDarkMode(initialState, darkModeConfig);
 
 	return (
 		<div className="dark-mode-toggle">
