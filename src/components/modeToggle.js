0d74f227b258d34fe108c83f9a4778a7e59a0e92
StudioMic <jnl@studiomic.net>
2024-03-28 08:03:06 +0900

    0328

---
+++ src/components/modeToggle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Toggle from './modetoggleButton.js';
+
+const DarkModeToggle = () => {
+	// 
+	
+	// const darkMode = useDarkMode(initialState, darkModeConfig);
+
+	return (
+		);
+	};
+	
+	export default DarkModeToggle;
