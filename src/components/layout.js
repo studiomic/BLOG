865b384f67b75191b3ed343394985ef4bde5318e
StudioMic <jnl@studiomic.net>
2024-03-30 15:18:21 +0900

    0330

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,14 +3,12 @@ import '../styles/variables.scss'
 import '../styles/global.scss'
 import Navigation from './navigation'
 import Footer from './footer'
-import Head from './head'
 
 class Template extends React.Component {
   render() {
     const { children } = this.props
     return (
       <>
-      <Head />
         <Navigation />
         <main>{children}</main>
         <Footer />
