e01e8b7f65800b07935b6f4ce4ac7cbeba837423
StudioMic <jnl@studiomic.net>
2024-04-05 11:00:50 +0900

    0405w

--- src/components/intoro-work.js
+++ src/components/intoro-work.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import * as styles from '../styles/intoro-work.module.scss'
+import * as styles from '../styles/work.module.scss'
 
 const Introduction = () => (
   <div className={styles.intoro} loading="lazy">
     <article>
-      <h1 className={styles.title}>Blog et Gatsby Pages de Cerise</h1>
+      <h1 className={styles.title}>Design Work</h1>
       <p>Je suis Designer et Web Developer</p>
     </article>
   </div>
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,18 +1,51 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Intoro from '../components/intoro-work'
-import * as styles from '../styles/about.module.scss'
-// import IMG from "../asset/img/sakana.png"
+
+// import styles from '../styles/work.module.scss'
+import * as styles from '../styles/work.module.scss'
+import IMG from "../asset/img/Flow.png"
+
 // import SNS from "../asset/svg/iconmonstr-instagram-11.svg"
 // import Seo from '../components/seo'
 
 export default function about() {
 	return (
 		<Layout>
-			<Intoro />
-			{/* <div class="wave">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path d="M0,64L80,85.3C160,107,320,149,480,144C640,139,800,85,960,58.7C1120,32,1280,32,1360,32L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"/></svg>
-			</div> */}
+			{/* <Intoro /> */}
+			<div className={styles.wrapper}>
+				<div className={styles.item1}>
+					WORK.1
+				</div>
+				<div className={styles.item2}>2</div>
+				<div className={styles.item3}></div>
+				<div className={styles.item4}>
+					
+
+				{/* <img src={IMG} alt="" width={100} /> */}
+
+				</div>
+				<div className={styles.item5}></div>
+				<div className={styles.item6}>6</div>
+				<div className={styles.item7}>7</div>
+				
+			</div>
+
+
+
+				{/* <div className={styles.item6}>6</div>
+				<div className={styles.item7}>7</div>
+				<div className={styles.item8}>8</div>
+				<div className={styles.item9}>9</div> */}
+
+
+
+
+
+
+
+
+
 
 
 			<article className={styles.article}>
