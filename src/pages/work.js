418e44c2ce287087c638c7fd720e201cade16775
StudioMic <jnl@studiomic.net>
2024-04-06 03:41:17 +0900

    0406

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from './../components/layout'
 import Intoro from './../components/intoro-work'
-import { StaticImage } from 'gatsby-plugin-image'
+// import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from './../styles/work.module.scss'
 
 export default function work() {
@@ -11,73 +11,59 @@ export default function work() {
 			<div className={styles.wrapper}>
 				<div className={styles.item1}>
 					<h2>WORK</h2>
+					<p>1</p>
 				</div>
-				<div className={styles.item2}>2</div>
-				<div className={styles.item3}></div>
+				<div className={styles.item2}>
+					<p>2</p>
+				</div>
+				<div className={styles.item3}>
+					<p>33333</p>
+				</div>
 				<div className={styles.item4}>
+					<p>4444</p>
 					
 
 
 				</div>
-				<div className={styles.item5}></div>
-				<div className={styles.item6}>
-					<div className={styles.figset}>
-						<figure className={styles.workimg}>
-							<StaticImage
-								src="../asset/workimg/BSC.png"
-								alt="Hero-image"
-								placeholder="BLURRED"
-								quality="100"
-							/>
-						</figure>
-						<figure className={styles.workimg}>
-							<StaticImage
-								src="../asset/workimg/PTM.png"
-								alt="Hero-image"
-								placeholder="BLURRED"
-								quality="100"
-							/>
-						</figure>
-						<figure className={styles.workimg}>
-							<StaticImage
-								src="../asset/workimg/tarao1.png"
-								alt="Hero-image"
-								placeholder="BLURRED"
-								quality="100"
-							/>
-						</figure>
-						<figure className={styles.workimg}>
-							<StaticImage
-								src="../asset/workimg/kyoudo.png"
-								alt="Hero-image"
-								placeholder="BLURRED"
-								quality="100"
-							/>
-						</figure>
-						<figure className={styles.workimg}>
-							<StaticImage
-								src="../asset/workimg/takehiko.png"
-								alt="Hero-image"
-								placeholder="BLURRED"
-								quality="100"
-							/>
-						</figure>
-					</div>
+				<div className={styles.item5}>
+					<p>555</p>
 				</div>
 
-				<div className={styles.item7}>7</div>
+				<div className={styles.item6}>
+					<p>666</p>
+
+					<div className={styles.markers}>
+						{/* <button as="a" href="#carousel_01">Photo.1</button> */}
+						
+						{/* <button onClick="location.href='#carousel_01'" />
+						<label htmlFor="carousel_01" for="carousel_01">Photo.1</label> */}
+					</div>
+
+				</div>
+
+
+				<div className={styles.item7}>
+					<p>777</p>
+
+
+		
+
+
+				</div>
+				<div className={styles.item8}>
+					<p>888</p>
+
+				</div>
+
+				<div className={styles.item9}>
+				<p>999</p>
+
+				</div>
+				<div className={styles.item10}>
+				<p>101010</p>
+				</div>
 				
 			</div>
-
-
-
-
-				{/* <div className={styles.item6}>6</div>
-				<div className={styles.item7}>7</div>
-				<div className={styles.item8}>8</div>
-				<div className={styles.item9}>9</div> */}
-
-
 		</Layout>
 	)
 }
