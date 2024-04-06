f2bb97c02d07e0732d609fc1666977148703e3d7
StudioMic <jnl@studiomic.net>
2024-04-06 16:30:38 +0900

    0406

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,2 @@
 import './src/styles/variables.scss';
 import './src/styles/global.scss';
-// import './src/styles/css/sample.css';
-
--- src/components/intoro-work.js
+++ src/components/intoro-work.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import * as styles from '../styles/work.module.scss'
+import * as styles from '../styles/components/intoro-work.module.scss'
 
 const Introduction = () => (
   <div className={styles.intoro} loading="lazy">
-    <article>
-      <h1 className={styles.title}>Design Work</h1>
-      <p>Je suis Designer et Web Developer</p>
-    </article>
+    <div className={styles.grid}>
+      <section>
+
+      </section>
+      <section>
+        <article>
+          <h1 className={styles.title}>Design Work</h1>
+          <p>Je suis Designer et Web Developer</p>
+        </article>
+      </section>
+      <section>
+
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+    
+    </div>
   </div>
 )
 
 export default Introduction
-
