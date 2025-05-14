2ad567c36562aa53175d93f5acffa36d462027dd
StudioMic <jnl@studiomic.net>
2025-05-14 10:33:59 +0900

    0514

--- src/pages/new-index.js
+++ src/pages/new-index.js
@@ -11,34 +11,32 @@ export default function work() {
 		<Layout>
 				<section className={styles.container}>
 					<div className={styles.layoutGrid}>
-						<div className={styles.grid}>1</div>
-						<div className={styles.grid}>2</div>
-						<div className={styles.grid}>3</div>
-						<div className={styles.grid}>4</div>
-						<div className={styles.grid}>5</div>
+						<div className={styles.headers}>
+							<header>
+								<h1>Design Deeply</h1>
+								<h2>Web designer.</h2>
+								<p>Development 26th</p>
+								<h3>Sakura Iizuka</h3>
+								<p>スタジオミック</p>
+								<h4>StudioMic</h4>
+							</header>
+						</div>
+						<div className={styles.rowline1}></div>
+						<div className={styles.rowline2}></div>
+						<div className={styles.rowline3}></div>
+
+						<div className={styles.free5}>5</div>
 						<div className={styles.grid}>6</div>
 						<div className={styles.grid}>7</div>
 						<div className={styles.grid}>8</div>
-						<div className={styles.grid}>9</div>
-						<div className={styles.grid}>10</div>
-						<div className={styles.grid}>11</div>
-						<div className={styles.grid}>12</div>
-						<div className={styles.grid}>13</div>
-						<div className={styles.grid}>14</div>
-						<div className={styles.grid}>15</div>
+
+						
 						
 			</div>
 
 
 
-					<header>
-						<h1>Web designer.</h1>
-						<p>Development 26th</p>
-						<h2>Sakura Iiduka</h2>
-						<h2>StudioMic</h2>
-
-					</header>
-
+					
 					
 					<article className={styles.wrapper}>
 						
