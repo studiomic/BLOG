b48b4bea35a071d5d076faf08a86eb655303fb21
StudioMic <jnl@studiomic.net>
2025-05-26 07:58:43 +0900

    progressbar

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -2,10 +2,7 @@ import React from 'react'
 import * as styles from '../styles/layout/navigation.module.scss'
 
 const Progressbar = () => (
-  <div Name={styles.progressbar}>
-    jdiqweoqodiwq
-    dqpwdjqp
-  </div>
+  <div className={styles.progressbar}></div>
 )
 
 export default Progressbar
