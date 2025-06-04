d74870a1fcdf5e6c0bc991b2c35a2b9f0f7fdf64
StudioMic <jnl@studiomic.net>
2025-06-04 16:19:33 +0900

    0604

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Pagination = ({ pageContext }) => {
-    const { numPages } = pageContext
-    return (
-        <h2>
-            {Array.from({ length: numPages }, (_, i) => (
-                <Link key={`pagination-number${i + 1}`} to={`/${i === 0 ? "" : i + 1}`}>
-                {i + 1}
-                </Link>
-            ))}
-        </h2>
-    )
+	const { numPages } = pageContext
+	return (
+		<div>
+			{Array.from({ length: numPages }, (_, i) => (
+					<Link key={`pagination-number${i + 1}`} to={`/${i === 0 ? "" : i + 1}`}>
+					{i + 1}
+					</Link>
+			))}
+		</div>
+	)
 }
-
 export default Pagination
