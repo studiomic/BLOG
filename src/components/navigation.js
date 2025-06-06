f0ca75d86ca8edb33ab2c2a2be15c495c5581b3e
StudioMic <jnl@studiomic.net>
2025-06-06 15:20:07 +0900

    0606

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -18,17 +18,6 @@ const Navigation = () => (
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/2/" activeClassName="active">
-        Page2
-        </Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/3/" activeClassName="active">
-        Page3
-        </Link>
-      </li>
-
-      <li className={styles.navigationItem}>
         <ModeButton />
       </li>
     </ul>
---
+++ src/pages/notes/note-14-ternary-operator.md
@@ -0,0 +1,10 @@
+---
+title: "Gatsbyページネーションと三項演算子の話"
+date: "2025-06-06 22:00:00"
+slug: 'ternary-operator-14'
+description: "わかってみるとスマートだったという理解"
+book:
+music:
+# featuredImage: "asset/Dependabot.avif"
+featuredImage: "asset/ternary-operator-14.png"
+---
